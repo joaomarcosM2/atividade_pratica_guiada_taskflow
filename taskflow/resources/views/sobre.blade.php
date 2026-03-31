d2b0def7f9330473febbf30660f90c2e52356087
Joao Marcos <joao6137@aluno.laboro.edu.br>
2026-03-31 13:39:04 -0300

    configuração do routes/web.php/sobre executada com sucesso!

--- taskflow/resources/views/sobre.blade.php
+++ taskflow/resources/views/sobre.blade.php
@@ -1,5 +1,5 @@
 <h1>Sobre o Taskflow</h1>
 <p>Esta é a página sobre o sistema de tarefas.</p>
 
-  /*  !!!  Análise e desenvolvimento de sistemas 5º período.
+  /*  !!!  Análise e desenvolvimento de sistemas 5º período. </br>
         Alunos: Steffany Fernandes, João Marcos Mendes e Leanderson.   !!!  */
--- taskflow/routes/web.php
+++ taskflow/routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+# Atividade Pratica / Rota Sobre
+use App\Http\Controllers\SobreController;
+Route::get('/sobre', [SobreController::class, 'index']);
+
+# Aula do professor / Rota teste
+Route::get('/teste', function () {
+return "Funcionando!";
+});
