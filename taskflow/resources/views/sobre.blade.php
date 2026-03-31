0cbdee6a30d1313c407427e92b799caae00af894
Joao Marcos <joao6137@aluno.laboro.edu.br>
2026-03-31 13:27:05 -0300

    configuração do resources/views e do controllers/sobre executada com sucesso!

---
+++ taskflow/resources/views/sobre.blade.php
@@ -0,0 +1,5 @@
+<h1>Sobre o Taskflow</h1>
+<p>Esta é a página sobre o sistema de tarefas.</p>
+
+  /*  !!!  Análise e desenvolvimento de sistemas 5º período.
+        Alunos: Steffany Fernandes, João Marcos Mendes e Leanderson.   !!!  */
